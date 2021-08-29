5495cdc64475fcfb447fc983c919e416b37b04e0
firgia <developer@nitrogia.com>
2021-08-29 21:50:17 +0700

    add header

--- file_manager/lib/app/features/dashboard/cloud/views/screens/cloud_screen.dart
+++ file_manager/lib/app/features/dashboard/cloud/views/screens/cloud_screen.dart
@@ -1,5 +1,7 @@
 library cloud;
 
+import 'package:file_manager/app/constans/app_constants.dart';
+import 'package:file_manager/app/shared_components/search_button.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -9,14 +11,30 @@ part '../../bindings/cloud_binding.dart';
 // controller
 part '../../controllers/cloud_controller.dart';
 
+// component
+part '../components/header.dart';
+
 class CloudScreen extends GetView<CloudController> {
   const CloudScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("cloud"),
+      body: SafeArea(
+        child: CustomScrollView(
+          physics: BouncingScrollPhysics(),
+          slivers: [
+            SliverFillRemaining(
+              child: Column(children: [
+                Padding(
+                  padding: const EdgeInsets.all(kDefaultSpacing),
+                  child: _Header(),
+                ),
+              ]),
+              hasScrollBody: false,
+            )
+          ],
+        ),
       ),
     );
   }
